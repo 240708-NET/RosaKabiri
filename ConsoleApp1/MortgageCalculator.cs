ecfd5c8c59c16871482cb3d4aaa5a7f12586a284
Rosakab <akkabiri2003@gmail.com>
2024-07-11 17:24:57 -0700

    creating the app with two classes and the story

---
+++ ConsoleApp1/MortgageCalculator.cs
@@ -0,0 +1,54 @@
+
+
+// The app will calculate the monthly mortgage.
+// it needs:
+//1- three variables: Loan term, Loan amount and annual interest
+//2- constructor to initialize these variables.
+//3- a method to perform and calculate mortgage
+    // loan term should be multiplied by 12, which is the number of months in a year
+    // annual interest should be converted to monthly interest
+//4- a formula for calculating
+
+
+namespace MortgageCalculator;
+
+
+public class MortgageCalculator
+{
+ public int LoanTerm { get; set; }
+ public double AnnualInterest{ get; set; }
+ public double LoanAmount{get; set; }
+
+
+
+
+public MortgageCalculator(double loanAmount, double annualInterest, int loanTerm)
+        {
+            LoanAmount = loanAmount;
+            AnnualInterest = annualInterest;
+            LoanTerm = loanTerm;
+        }
+
+
+ public double CalculatePaymentMonthly()
+        {
+        int totalPayments = LoanTerm * 12;
+        double monthlyInterest = (AnnualInterest / 100) / 12;
+         
+        double paymentMonthly = LoanAmount * monthlyInterest *
+        Math.Pow(1 + monthlyInterest, totalPayments) /
+        (Math.Pow(1 + monthlyInterest, totalPayments) - 1);
+
+
+            return paymentMonthly;
+        }
+
+
+}
+
+
+
+
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using MortgageCalculator;
+
+
+ class Program
+{
+
+
+      public static void Main(string[] args){
+
+             double loanAmount = 100000; 
+            double annualInterest = 5; 
+            int loanTerm = 30; 
+     
+        MortgageCalculator.MortgageCalculator cal = new MortgageCalculator.MortgageCalculator(loanAmount, annualInterest, loanTerm);
+
+
+        Console.WriteLine("Mortgage Calculator App\n");
+        Console.WriteLine(" Please Enter the loan amount: ");
+
+       
+            while (!double.TryParse(Console.ReadLine(), out loanAmount))
+            {
+                Console.WriteLine(" Please enter a valid loan amount.");
+            }
+
+
+        Console.Write("Please Enter the annual interest rate ");
+     
+            while (!double.TryParse(Console.ReadLine(), out annualInterest))
+            {
+                Console.WriteLine(" Please enter a valid annual interest rate.");
+            }
+
+          
+        Console.Write("Please Enter the loan term: ");
+
+       
+            while (!int.TryParse(Console.ReadLine(), out loanTerm))
+            {
+                Console.WriteLine("Please enter a valid loan term in years.");
+            }
+
+
+double paymentMonthly= cal.CalculatePaymentMonthly();
+
+ Console.WriteLine("Monthly Payment: " + "$" + paymentMonthly);
+}
+}
